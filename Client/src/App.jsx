d2c0e20d90dec2f2ee43e36fcbf23666cf64e160
Divyansh Sharma <divyanshsharma4507@gmail.com>
2024-07-22 19:43:09 +0530

    First commit

---
+++ Client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () =>{
+return(
+  <> 
+ <h1 className="text-3xl font-bold underline">
+      Build here niggas
+    </h1>
+  </>
+)
+}
+
+export default App;
