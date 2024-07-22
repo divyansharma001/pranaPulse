f3a92e01238f3003f8df3345eaf7dc9579a43833
itsharshitrwt <harshit.rawat1804@gmail.com>
2024-07-23 00:48:33 +0530

    basic-front-design

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,11 +1,15 @@
 import Front from "./components/Body/Front";
+import Footer from "./components/footer/Footer";
 import NavBar from "./components/header/NavBar";
+
 
 function App() {
   return (
     <>
       <NavBar/>
       <Front/>
+      <Footer/>
+      
     </>
   );
 }
---
+++ Client/src/components/footer/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const footer = () => {
+  return (
+    <div className='h-24 w-full bg-[#0584AB] text-white text-center font-semibold text-3xl'></div>
+  )
+}
+
+export default footer
