78244786e5aed4e7acf481330dc5737933dc692a
Shushant-Priyadarshi <profshushant@gmail.com>
2024-07-26 18:22:33 +0530

    animations done

--- Client/src/components/about/About.jsx
+++ Client/src/components/about/About.jsx
@@ -1,9 +1,28 @@
-
+import { motion } from "framer-motion"
 
 const About = () => {
   return (
        <>
-       About</>
+       About
+       
+
+       <motion.div
+        initial={{ y: "-100%" }}
+        animate={{ y: "100%" }}
+        transition={{ duration: 2, ease: [0.2, 1, 0.2, 1] }}
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100vh",
+          backgroundColor: "#E0E6F9",
+          zIndex: 20,
+        }}
+      ></motion.div>
+
+      
+       </>
 
   )
 }
