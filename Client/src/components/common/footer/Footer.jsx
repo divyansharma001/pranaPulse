ab0b840d5f5f80417bf4c0c31451a23be61d1829
Divyansh Sharma <divyanshsharma4507@gmail.com>
2024-07-27 18:22:42 +0530

    Some ui improvement

--- Client/src/components/common/footer/Footer.jsx
+++ Client/src/components/common/footer/Footer.jsx
@@ -1,9 +1,14 @@
+
+
 const footer = () => {
   return (
     <div className="w-full py-[20px] bg-gradient-to-b from-[#D9DFFE] to-[#EDE8F5] mt-auto ">
       <div className="flex justify-between items-center  p-2 md:p-4  ">
-        <div className="flex gap-1 items-center text-sm md:text-base ">
-          💓 Made with MERN,{<br></br>}Tailwind & Framer Motion
+        <div className="flex gap-1 items-center text-sm md:text-base flex-col">
+        
+        
+          <a href="https://github.com/divyansharma001/pranaPulse">⭐Star us on Github</a>
+          
         </div>
 
         <div className="flex gap-4 text-2xl md:text-3xl mr-3">
